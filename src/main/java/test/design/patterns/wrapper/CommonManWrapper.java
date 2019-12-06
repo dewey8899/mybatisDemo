c47923164df25578e196d0273603a9a582d20352
dewey <1246818050@qq.com>
2019-12-06 15:58:01 +0800

    添加AOP依赖

--- src/main/java/test/design/patterns/wrapper/CommonManWrapper.java
+++ src/main/java/test/design/patterns/wrapper/CommonManWrapper.java
@@ -3,6 +3,8 @@ package test.design.patterns.wrapper;
 /**
  * Created by deweydu
  * Date on 2019/12/5 14:52
+ * function: 我们需要先提供一个抽象装饰者类，这个类封装需要增强的原始对象，然后，根据不同的功能点提供其子类。
+ *
  */
 public abstract class CommonManWrapper implements Man{
     private Man man;
